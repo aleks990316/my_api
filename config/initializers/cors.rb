b0cbc9357da0b08328bc6a83101597990e2334cf
aleks990316 <aleksandr.holodov.99@mail.ru>
2021-07-10 20:12:09 +0700

    update gem

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,12 +14,13 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://arrogant-worms-17513.herokuapp.com'
+      origins '*'
   
       resource '*',
         headers: :any,
-        methods: [:get]
+        methods: [:get, :post, :patch, :put]
     end
   end
