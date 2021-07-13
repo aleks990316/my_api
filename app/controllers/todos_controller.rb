2436bfd688fe3780719eabc38e0f7dbb99253a24
aleks990316 <aleksandr.holodov.99@mail.ru>
2021-07-13 19:56:54 +0700

    update todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
     def update
         todo = Todo.find_by(id: params[:id], id_project: params[:project_id])
         if todo
-            todo.update(iscompleted: !:iscompleted)
+            todo.update(iscompleted: !todo.iscompleted)
         end
     end
     def create
